4f3cd7bbec939315f33be589ed30ddfa6e2550be
a.dergachev <bestacc@mail.ru>
2022-09-15 20:47:22 +0300

    Created string select algorithm

--- Hometask/Program.cs
+++ Hometask/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Метод выбора значений массива длинной менее или равной 3
+string[] StrSelect(string[] array, int arg)
+{
+    string[] newarr = new string[array.Length];
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= arg)
+        {
+            newarr[count] = array[i];
+            count++;
+        }
+    }
+    Array.Resize(ref newarr, count);
+    return newarr;
+}
+
